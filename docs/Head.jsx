c1c5f4692907fc3359a65aa655afa271c54ff531
jxnblk <jacksonblack@gmail.com>
2016-10-22 16:23:29 -0400

    Add image for twitter card

--- docs/Head.jsx
+++ docs/Head.jsx
@@ -3,11 +3,11 @@
   <meta name='viewport' content='width=device-width, initial-scale=1' />
   <title>{props.title}</title>
   <meta name='description' content={props.description} />
-  <meta name='twitter:card' content='summary' />
+  <meta name='twitter:card' content='summary_large_image' />
   <meta name='twitter:site' content='@jxnblk' />
   <meta name='twitter:title' content='Ejsx' />
   <meta name='twitter:description' content={props.description} />
-  <meta name='twitter:image' content='' />
+  <meta name='twitter:image' content='http://jxnblk.com/ejsx/card.png' />
   <link rel='stylesheet' href='https://unpkg.com/type-system@1.0.0-beta.2/type-system.css' />
   <link rel='stylesheet' href='https://unpkg.com/open-color@1.4.0/open-color.css' />
   <Style />
